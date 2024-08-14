e7939e37c08d82d5e417c87c9fe9e6c0c884aa2a
bonhart67 <lozsek.peter@gmail.com>
2024-08-14 19:00:10 +0200

    nvim config with gruvbox and basic configs

---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,19 @@
+local nnoremap = function(shortcut, result)
+  vim.keymap.set('n', shortcut, result, {noremap = true, silent = true})
+end
+
+local vnoremap = function(shortcut, result)
+  vim.keymap.set('v', shortcut, result, {noremap = true, silent = true})
+end
+
+-- Navigate vim panes better
+nnoremap('<c-k>', ':wincmd k<CR>')
+nnoremap('<c-j>', ':wincmd j<CR>')
+nnoremap('<c-h>', ':wincmd h<CR>')
+nnoremap('<c-l>', ':wincmd l<CR>')
+
+vnoremap('<', '<gv')
+vnoremap('>', '>gv')
+
+nnoremap('<leader><esc>', ':nohlsearch<CR>')
+nnoremap('<leader>q', ':q<CR>')
---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,21 @@
+local o = vim.opt
+
+vim.g.mapleader = " "
+
+o.clipboard = 'unnamedplus'
+o.completeopt = {'menu', 'menuone', 'noselect'}
+
+o.tabstop = 2
+o.softtabstop = 2
+o.shiftwidth = 2
+o.expandtab = true
+
+o.number = true
+o.relativenumber = true
+o.splitright = true
+
+o.incsearch = true
+o.ignorecase = true
+o.smartcase = true
+
+o.swapfile = false

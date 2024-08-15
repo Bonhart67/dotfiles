87a7ff5dd84c083d46a3de0e0e9901df701d1a4f
bonhart67 <lozsek.peter@gmail.com>
2024-08-15 23:11:20 +0200

    xrandr script and battery fix

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -4,7 +4,7 @@ set_bg
 
 while true; do 
     CURRENT_TIME=$(date +%H:%M)
-    BATT=$(acpi | awk '{print $5}')
+    BATT=$(echo $(acpi | cut -d ',' -f2))
     xsetroot -name "[$BATT] [$CURRENT_TIME]";
     sleep 1;
 done  &

57b31c7585c41279eccc28a6345ace4489125e61
bonhart67 <lozsek.peter@gmail.com>
2024-08-16 06:03:45 +0200

    update autostart for non-laptop

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -4,7 +4,13 @@ set_bg
 
 while true; do 
     CURRENT_TIME=$(date +%H:%M)
-    BATT=$(echo $(acpi | cut -d ',' -f2))
-    xsetroot -name "[$BATT] [$CURRENT_TIME]";
+    if [ ! -z $(command -v acpi) ]; then
+      BATT=$(echo $(acpi | cut -d ',' -f2))
+      xsetroot -name "[$BATT] [$CURRENT_TIME]";
+      echo "yes"
+    else
+      xsetroot -name "[$CURRENT_TIME]";
+      echo "no"
+    fi
     sleep 1;
 done  &

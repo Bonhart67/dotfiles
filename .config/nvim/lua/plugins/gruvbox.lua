e6fe72a63571e9d5d226c799ad9369bc8303a9f7
bonhart67 <lozsek.peter@gmail.com>
2024-08-16 04:11:14 +0200

    add noice

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -2,6 +2,14 @@ return {
   'ellisonleao/gruvbox.nvim',
   priority = 1000,
   config = function()
+    require('gruvbox').setup({
+      overrides = {
+        NoiceCmdlinePopupBorderCmdline = { bg = '#282828' },
+        NoiceCmdlineIconCmdline = { bg = '#282828' },
+        NoiceCmdlinePopupBorderHelp = { bg = '#282828' },
+        NoiceCmdlineIconHelp = { bg = '#282828' },
+      },
+    })
     vim.cmd.colorscheme('gruvbox')
   end,
 }

881d0d4f5ef46a170456574e1ba6332336ab56a1
bonhart67 <lozsek.peter@gmail.com>
2024-09-02 11:03:08 +0200

    fix noice colors and font

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -5,11 +5,13 @@ return {
 	opts = {
 		overrides = {
 			NoiceCmdlinePopupBorderCmdline = { bg = "#282828" },
-			NoiceCmdlineIconCmdline = { bg = "#282828" },
 			NoiceCmdlinePopupBorderHelp = { bg = "#282828" },
-			NoiceCmdlineIconHelp = { bg = "#282828" },
 			NoiceCmdlinePopupBorderLua = { bg = "#282828" },
+			NoiceCmdlinePopupBorderSearch = { bg = "#282828" },
+			NoiceCmdlineIconCmdline = { bg = "#282828" },
+			NoiceCmdlineIconHelp = { bg = "#282828" },
 			NoiceCmdlineIconLua = { bg = "#282828" },
+			NoiceCmdlineIconSearch = { bg = "#282828" },
 		},
 	},
 	config = function(_, opts)

f7c14403c380f81035318fd1129f44758bcad952
bonhart67 <lozsek.peter@gmail.com>
2025-06-02 15:43:15 +0200

    gruvbox

---
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    opt = {
+      contrast = "hard",
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
